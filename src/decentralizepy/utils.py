a9b2497e55ca0dfa3c1289a00e7f8166c095a6df
Marie Reinbigler <marie.reinbigler@telecom-sudparis.eu>
2025-01-20 18:49:20 +0100

    Added an option to the code entrypoint

--- src/decentralizepy/utils.py
+++ src/decentralizepy/utils.py
@@ -86,6 +86,7 @@ def get_args():
     parser.add_argument("-sm", "--server_machine", type=int, default=0)
     parser.add_argument("-sr", "--server_rank", type=int, default=-1)
     parser.add_argument("-wr", "--working_rate", type=float, default=1.0)
+    parser.add_argument("-ir", "--init_res", type=float, default=0.)
 
     args = parser.parse_args()
     return args
@@ -118,6 +119,7 @@ def write_args(args, path):
         "train_evaluate_after": args.train_evaluate_after,
         "reset_optimizer": args.reset_optimizer,
         "working_rate": args.working_rate,
+        "init_res": args.init_res,
     }
     with open(os.path.join(path, "args.json"), "w") as of:
         json.dump(data, of)
